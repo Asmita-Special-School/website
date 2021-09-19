95d4d0f597377c143df9701939289d85decb0986
anirudhbelwadi <anirudh.belwadi@gmail.com>
2021-09-19 19:44:29 +0400

    build 1.0

---
+++ assets/scripts/index.js
@@ -0,0 +1,9 @@
+$(window).scroll(function () {
+    var scroll_amount = $(window).scrollTop();
+    if (scroll_amount > 50) {
+        $(".navbar").fadeIn();
+    }
+    if (scroll_amount < 50) {
+        $(".navbar").fadeOut();
+    }
+});
